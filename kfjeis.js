f918b4044f420955f20eb1e8a7ac034b0715ccd1
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-31 12:40:39 +0900

    Add files via upload

---
+++ kfjeis.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    const fleftDiv = document.querySelector('.fleft');
+        if (fleftDiv) {
+            if (!fleftDiv.innerHTML.includes('Sui Design')) {
+                const pElement = document.createElement('p');
+                pElement.textContent = 'Copyright Sui Design';
+                fleftDiv.appendChild(pElement);
+            }
+        } else {
+            window.location.href = 'https://suiblog.site';
+        }
+});
+
+
+
+
+
+    
+
